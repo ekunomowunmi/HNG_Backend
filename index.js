64da71c1ef09886fafd6d0d7cf69428b9734ad72
Omowunmi <oekun@seamfix.com>
2022-11-04 14:28:39 +0100

    add post

--- index.js
+++ index.js
@@ -32,7 +32,36 @@ app.use(bodyParser.json());
     res.json(user)
   } );
 
-
+  app.post("/perform-operation",(req,res)=>{
+    let body = req.body;
+    let result = 0;
+    if(body){
+        if(typeof(body.x) !== 'number' || typeof(body.y) !== 'number'
+        ){
+            return res.status(400).json({ error:'Invalid Numbers' })
+        }
+        if( !Number.isInteger(body.x) ||  !Number.isInteger(body.y)){
+            {
+                return res.status(400).json({ error:'Invalid : Must be an Integer' })
+            }
+        }
+        
+        switch(body.operation_type){
+            case 'addition':
+                result = body.x + body.y;
+                break;
+            case 'subtraction':
+                result = body.x - body.y;
+                break;
+            case 'multiplication':
+                result = body.x * body.y;
+                break;
+            default:
+                return res.status(500).json({error: 'Invalid Operation Type'});
+        }
+        return res.status(200).json({"slackUsername":'Omowunmi Ekun', "result":result, "operation_type":body.operation_type})
+    }
+  })
 
   
 
